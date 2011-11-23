ada14304de76f8fe09e5c5755d96ccb7ebfc6b84
ludo.visser <ludo.visser@localhost>
2011-11-23 16:33:43 +0100

    Made a mistake in bit logic...

--- src/mbed/mbed.c
+++ src/mbed/mbed.c
@@ -24,48 +24,48 @@ void initSys(uint32_t flags) {
 	mbedStatus = 0x0;
 
 	/* Intialize timers. */
-	if ( (mbedStatus ^ MBED_TIMER0_INIT) && (flags & INIT_TIMER0) ) {
+	if ( !(mbedStatus & MBED_TIMER0_INIT) && (flags & INIT_TIMER0) ) {
 		initTimer(MBED_TIMER0);
 	}
 
-	if ( (mbedStatus ^ MBED_TIMER1_INIT) && (flags & INIT_TIMER1) ) {
+	if ( !(mbedStatus & MBED_TIMER1_INIT) && (flags & INIT_TIMER1) ) {
 		initTimer(MBED_TIMER1);
 	}
 
-	if ( (mbedStatus ^ MBED_TIMER2_INIT) && (flags & INIT_TIMER2) ) {
+	if ( !(mbedStatus & MBED_TIMER2_INIT) && (flags & INIT_TIMER2) ) {
 		initTimer(MBED_TIMER2);
 	}
 
-	if ( (mbedStatus ^ MBED_TIMER3_INIT) && (flags & INIT_TIMER3) ) {
+	if ( !(mbedStatus & MBED_TIMER3_INIT) && (flags & INIT_TIMER3) ) {
 		initTimer(MBED_TIMER3);
 	}
 
 	/* Initialize peripherals. */
-	if ( (mbedStatus ^ MBED_MUSB_INIT) && (flags & INIT_MUSB) ) {
+	if ( !(mbedStatus & MBED_MUSB_INIT) && (flags & INIT_MUSB) ) {
 		initMiniUSB();
 	}
 
-	if ( (mbedStatus ^ MBED_SERIAL0_INIT) && (flags & INIT_SERIAL0) ) {
+	if ( !(mbedStatus & MBED_SERIAL0_INIT) && (flags & INIT_SERIAL0) ) {
 		initSerial(MBED_SERIAL0);
 	}
 
-	if ( (mbedStatus ^ MBED_SERIAL1_INIT) && (flags & INIT_SERIAL1) ) {
+	if ( !(mbedStatus & MBED_SERIAL1_INIT) && (flags & INIT_SERIAL1) ) {
 		initSerial(MBED_SERIAL1);
 	}
 
-	if ( (mbedStatus ^ MBED_SERIAL2_INIT) && (flags & INIT_SERIAL2) ) {
+	if ( !(mbedStatus & MBED_SERIAL2_INIT) && (flags & INIT_SERIAL2) ) {
 		initSerial(MBED_SERIAL2);
 	}
 
-	if ( (mbedStatus ^ MBED_CAN0_INIT) && (flags & INIT_CAN0) ) {
+	if ( !(mbedStatus & MBED_CAN0_INIT) && (flags & INIT_CAN0) ) {
 		initCAN(MBED_CAN0);
 	}
 
-	if ( (mbedStatus ^ MBED_CAN1_INIT) && (flags & INIT_CAN1) ) {
+	if ( !(mbedStatus & MBED_CAN1_INIT) && (flags & INIT_CAN1) ) {
 		initCAN(MBED_CAN1);
 	}
 
-	if ( (mbedStatus ^ MBED_LED_INIT) && (flags & INIT_LED) ) {
+	if ( !(mbedStatus & MBED_LED_INIT) && (flags & INIT_LED) ) {
 		initLED();
 		if (mbedStatus & MBED_TIMER0_INIT) {
 			ledOn(LED0 | LED1 | LED2 | LED3);
@@ -75,11 +75,11 @@ void initSys(uint32_t flags) {
 	}
 
 	/* Configure GPIO. */
-	if ( (mbedStatus ^ MBED_GPI_INIT) && (flags & INIT_GPI) ) {
+	if ( !(mbedStatus & MBED_GPI_INIT) && (flags & INIT_GPI) ) {
 		initGPInputs();
 	}
 
-	if ( (mbedStatus ^ MBED_GPO_INIT) && (flags & INIT_GPO) ) {
+	if ( !(mbedStatus & MBED_GPO_INIT) && (flags & INIT_GPO) ) {
 		initGPOutputs();
 	}
 
